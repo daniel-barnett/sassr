f3b44570bd02a24a4048209ab3aa2cb695ef28b6
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2018-05-04 18:32:56 +1200

    Filename argument

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,19 +6,20 @@
 using namespace Rcpp;
 
 // sass_compile_file
-NumericVector sass_compile_file(List options);
-RcppExport SEXP _sassr_sass_compile_file(SEXP optionsSEXP) {
+NumericVector sass_compile_file(CharacterVector filename_arg, List options);
+RcppExport SEXP _sassr_sass_compile_file(SEXP filename_argSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< CharacterVector >::type filename_arg(filename_argSEXP);
     Rcpp::traits::input_parameter< List >::type options(optionsSEXP);
-    rcpp_result_gen = Rcpp::wrap(sass_compile_file(options));
+    rcpp_result_gen = Rcpp::wrap(sass_compile_file(filename_arg, options));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_sassr_sass_compile_file", (DL_FUNC) &_sassr_sass_compile_file, 1},
+    {"_sassr_sass_compile_file", (DL_FUNC) &_sassr_sass_compile_file, 2},
     {NULL, NULL, 0}
 };
 
--- src/sassr_compile.cpp
+++ src/sassr_compile.cpp
@@ -12,25 +12,25 @@ int set_options(Sass_Options* ctx_opt, List opt_list) {
 }
 
 // [[Rcpp::export]]
-NumericVector sass_compile_file(List options = R_NilValue) {
-  const char* test_str = "test.scss";
-  
-  struct Sass_File_Context* file_ctx = sass_make_file_context(test_str);
+NumericVector sass_compile_file(CharacterVector filename_arg, List options = R_NilValue) {
+  const char* filename = as<const char*>(filename_arg);
+
+  struct Sass_File_Context* file_ctx = sass_make_file_context(filename);
   struct Sass_Context* ctx = sass_file_context_get_context(file_ctx);
   struct Sass_Options* ctx_opt = sass_context_get_options(ctx);
-  
+
   if (!options.isNULL()) {
     set_options(ctx_opt, options);
   }
-  
+
   int status = sass_compile_file_context(file_ctx);
-  
-  if (status == 0) 
+
+  if (status == 0)
     puts(sass_context_get_output_string(ctx));
-  else 
+  else
     puts(sass_context_get_error_message(ctx));
-  
+
   sass_delete_file_context(file_ctx);
-  
+
   return NumericVector::create(0, 1);
 }
