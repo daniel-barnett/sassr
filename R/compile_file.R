b35b1e52ec1234a02c6533ca790baaaa33678eb4
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2018-05-04 18:28:31 +1200

    Error checking in sass_compile()

--- R/compile_file.R
+++ R/compile_file.R
@@ -1,3 +1,13 @@
-sass_compile <- function(string, options = NULL) {
-  sass_compile_file(options = options)
+sass_compile <- function(string, file, options = NULL) {
+  if (all(missing(string), missing(file))) {
+    stop("A `string` or `file` must be passed to sass_compile()")
+  } else if (all(!missing(string), !missing(file))) {
+    stop("Only one of `string` or `file` must be passed to sass_compile()")
+  }
+
+  if (!missing(string)) {
+    ## sass_compile_string(string, options)
+  } else if (!missing(file)) {
+    sass_compile_file(options)
+  }
 }
